9d3dc308931e3434935f62dc15d3abc3d5e27091
Tatsuro Nakamura <me@tatsuroro.com>
2020-10-11 07:52:33 +0900

    Build page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Undefined - tatsuroro | Tatsuro Nakamura</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Exo:wght@400&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,64 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <span className={styles.title_packing}>U</span>ndefined
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className={styles.description}>tatsuroro | Tatsuro Nakamura</p>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <section>
+        <div className={styles.section_title}>
+          <span className={styles.tape}>About</span>
+        </div>
+        <div className={styles.section_body}>
+          <p>
+            Tatsuro Nakamura is a software developer living in Fukuoka, Japan.
+          </p>
+          <h3>Skills</h3>
+          <ul>
+            <li>TypeScript, JavaScript</li>
+            <li>Web Frontend Application Structure</li>
+            <li>UI, Interaction design</li>
+            <li>Facilitate, Coaching</li>
+          </ul>
+          <h3>Experience</h3>
+          <ul>
+            <li>Ubie, Inc. (2020-)</li>
+            <li>LINE Growth Technology Corporation (2019-2020)</li>
+            <li>Kaizen Platform, Inc. (2015-2019)</li>
+            <li>CyberAgent, Inc. (2012-2015)</li>
+            <li>
+              Amateur scriptwriter and stage director / Web designer (2005-2011)
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section>
+        <div className={styles.section_title}>
+          <span className={styles.tape}>Activities</span>
+        </div>
+        <div className={styles.section_body}>
+          <p>
+            <a href="https://tatsuroro.hateblo.jp/">Blog</a>
+          </p>
+          <p>
+            <a href="https://github.com/tatsuroro">GitHub</a>
+          </p>
+          <p>
+            <a href="https://twitter.com/tatsuroro">Twitter</a>
+          </p>
+          <p>
+            <a href="https://www.facebook.com/tatsuro.nk">Facebook</a>
+          </p>
+        </div>
+      </section>
+      <footer>
+        &copy; tatsuroro.com
       </footer>
     </div>
-  )
+  );
 }
